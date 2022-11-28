41041d7130ed9cfd143e82ef0090f5a5e30292b5
dlian7890 <dongpinglian7890@gmail.com>
2022-11-28 11:51:01 -0500

    init server

---
+++ server/models/playlist-model.js
@@ -0,0 +1,29 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const playlistSchema = new Schema(
+  {
+    name: { type: String, required: true },
+    ownerEmail: { type: String, required: true },
+    songs: {
+      type: [
+        {
+          title: String,
+          artist: String,
+          youTubeId: String,
+        },
+      ],
+      required: true,
+    },
+    isPublished: { type: Boolean, required: true },
+    publishDate: { type: Date, required: true },
+    listensCount: { type: Number, required: true },
+    likesCount: { type: Number, required: true },
+    dislikesCount: { type: Number, required: true },
+    comments: { type: [{ userName: String, text: String }], required: true },
+    interactedUsers: { type: [], required: true },
+  },
+  { timestamps: true }
+);
+
+module.exports = mongoose.model('Playlist', playlistSchema);
---
+++ server/routes/playlists-router.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const PlaylistController = require('../controllers/playlist-controller');
+const router = express.Router();
+const auth = require('../auth');
+
+router.post('/playlist', auth.verify, PlaylistController.createPlaylist);
+router.delete('/playlist/:id', auth.verify, PlaylistController.deletePlaylist);
+// router.get('/playlists', auth.verify, PlaylistController.getPlaylistByUser);
+// router.get('/playlists', PlaylistController.getPublishedPlaylists);
+router.put('/playlist/:id', auth.verify, PlaylistController.updatePlaylist);
+
+module.exports = router;
